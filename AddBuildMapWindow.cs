24d89c4ada30423d4daffc51a371d3b073352e38
joey1258 <1260216329@qq.com>
2016-10-08 16:35:45 +0800

    2016.10.8

---
+++ AddBuildMapWindow.cs
@@ -0,0 +1,306 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Utils;
+
+/// <summary>
+/// 打包文件类型枚举，可自行根据需要定义
+/// </summary>
+public enum SuffixEnum
+{
+    Prefab,
+    Png,
+    Csv,
+    Txt,
+    Lua
+}
+
+/// <summary>
+/// AssetBundle variant 属性枚举，可自行根据需要定义
+/// </summary>
+public enum VariantEnum
+{
+    None,
+    _1280x720,
+}
+
+/// <summary>
+/// AssetBundle variant 属性枚举，可自行根据需要定义
+/// </summary>
+public enum YN
+{
+    _true,
+    _false,
+}
+
+public class AddBuildMapWindow : EditorWindow
+{
+    /// <summary>
+    /// 总数
+    /// </summary>
+    int count = 0;
+
+    /// <summary>
+    /// bundle 名称 List
+    /// </summary>
+    List<string> bundleNameList = new List<string>();
+
+    /// <summary>
+    /// 文件类型 List
+    /// </summary>
+    List<SuffixEnum> suffixList = new List<SuffixEnum>();
+
+    /// <summary>
+    /// path List
+    /// </summary>
+    List<string> pathList = new List<string>();
+
+    Vector2 scrollValue = Vector2.zero;
+    VariantEnum variant = VariantEnum.None;
+    YN deleteExists = YN._false;
+    BuildTarget target = BuildTarget.StandaloneWindows;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    [MenuItem("BuildUtility/AddBuildMapUtility")]
+    static void SetAssetBundleNameExtension()
+    {
+        EditorWindow.GetWindow<AddBuildMapWindow>();
+    }
+
+    void OnGUI()
+    {
+        EditorGUILayout.BeginHorizontal();
+
+        if (GUILayout.Button("添加一项")) { AddItem(); }
+
+        if (GUILayout.Button("清除所有项")) { Clear(); }
+
+        EditorGUILayout.LabelField("Variant(非贴图类资源请设none):", GUILayout.Width(175));
+        variant = (VariantEnum)EditorGUILayout.EnumPopup(variant, GUILayout.Width(100));
+
+        if (GUILayout.Button("读取(csv)"))
+        {
+            Clear();
+
+            string path = Application.dataPath + "/Files/BuildMap_cvs/AssetBundleInfo.csv";
+            string content = File.ReadAllText(path);
+            string[] contents = content.Split(new string[] { "\r\n" }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            for (int i = 0; i < contents.Length; i++)
+            {
+                string[] a = contents[i].Split(',');
+                AddItem(a[0], StringToSuffixEnum(a[1]), a[2]);
+            }
+
+            variant = StringToVariantEnum(contents[0].Split(',')[3]);
+        }
+
+        if (GUILayout.Button("保存"))
+        {
+            Save();
+        }
+
+        if (GUILayout.Button("自动填写(所有选中的)"))
+        {
+            int startIndex = count;
+            for (int i = 0; i < Selection.objects.Length; i++)
+            {
+                AddItem();
+                AutoFill(startIndex, Selection.objects[i]);
+                startIndex++;
+            }
+        }
+
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.LabelField("注意：lua文件请以文件夹为单位进行选择！文件夹名即为包名！lua文件请不要混同其他类型文件存放！打包lua文件请点击Build Lua按钮，Build Lua Selected打包选中的文件夹下的lua文件;");
+        EditorGUILayout.LabelField("Build Lua All打包所有同级目录中的lua文件，打包其他文件请点击Build Asset按钮。按分辨率打包纹理时遇到报“Variant folder path cannot be empty”错误时请忽略，此为 U3D 5.3.X 版本的 bug，经实测不影响使用.");
+
+        scrollValue = EditorGUILayout.BeginScrollView(scrollValue);
+
+        for (int i = 0; i < count; i++)
+        {
+            EditorGUILayout.BeginVertical();
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField(i.ToString() + ".AB包名:", GUILayout.Width(70));
+            bundleNameList[i] = EditorGUILayout.TextField("", bundleNameList[i], GUILayout.Width(100));
+            EditorGUILayout.LabelField(" 类型:", GUILayout.Width(40));
+            suffixList[i] = (SuffixEnum)EditorGUILayout.EnumPopup(suffixList[i]);
+            EditorGUILayout.LabelField(" 路径:", GUILayout.Width(40));
+            pathList[i] = EditorGUILayout.TextField(pathList[i]);
+
+            if (GUILayout.Button("自动填写(单个)"))
+            {
+                AutoFill(i, Selection.objects[0]);
+            }
+            if (GUILayout.Button("log路径"))
+            {
+                Debug.Log(pathList[i]);
+            }
+            if (GUILayout.Button("删除该项"))
+            {
+                RemoveItem(i);
+            }
+            EditorGUILayout.EndHorizontal();
+            EditorGUILayout.EndVertical();
+        }
+
+        EditorGUILayout.EndScrollView();
+
+        EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.LabelField(" 是否删除已存在的其他文件:", GUILayout.Width(140));
+        deleteExists = (YN)EditorGUILayout.EnumPopup(deleteExists, GUILayout.Width(50));
+        EditorGUILayout.LabelField(" 平台:", GUILayout.Width(40));
+        target = (BuildTarget)EditorGUILayout.EnumPopup(target, GUILayout.Width(130));
+        if (GUILayout.Button("Build Lua Selected"))
+        {
+            Save();
+            if (deleteExists == YN._false) PackagerUtils.BuildLuaResource(false, false, target);
+            else PackagerUtils.BuildLuaResource(false, true, target);
+        }
+        if (GUILayout.Button("Build Lua All"))
+        {
+            Save();
+            if (deleteExists == YN._false) PackagerUtils.BuildLuaResource(true, false, target);
+            else PackagerUtils.BuildLuaResource(true, true, target);
+        }
+        if (GUILayout.Button("Build Asset"))
+        {
+            Save();
+            if (deleteExists == YN._false) PackagerUtils.BuildAssetResource(false, target);
+            else PackagerUtils.BuildAssetResource(true, target);
+        }
+        EditorGUILayout.EndHorizontal();
+    }
+
+    void Clear()
+    {
+        count = 0;
+        bundleNameList = new List<string>();
+        suffixList = new List<SuffixEnum>();
+        pathList = new List<string>();
+    }
+
+    /// <summary>
+    /// 添加一个 Item
+    /// </summary>
+    void AddItem(string bundleName = "", SuffixEnum suffix = SuffixEnum.Prefab, string path = "")
+    {
+        count++;
+        bundleNameList.Add(bundleName);
+        suffixList.Add(suffix);
+        pathList.Add(path);
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    void RemoveItem(int index)
+    {
+        count--;
+        bundleNameList.Remove(bundleNameList[index]);
+        suffixList.Remove(suffixList[index]);
+        pathList.Remove(pathList[index]);
+    }
+
+    /// <summary>
+    /// 为在 U3D 中选中的资源自动填写
+    /// </summary>
+    void AutoFill(int index, Object selectedObject)
+    {
+        string path = AssetDatabase.GetAssetPath(selectedObject);
+        bundleNameList[index] = path.Remove(0, path.LastIndexOf("/") + 1).ToLower() + AppDefine.ExtName;
+
+        string[] files = Directory.GetFiles(path);
+        string[] temp = files[0].Split('.');
+        suffixList[index] = StringToSuffixEnum("*." + temp[1]);
+
+        pathList[index] = path;
+    }
+
+    public static string SuffixEnumToString(SuffixEnum se)
+    {
+        switch (se)
+        {
+            case SuffixEnum.Prefab:
+                return "*.prefab";
+            case SuffixEnum.Png:
+                return "*.png";
+            case SuffixEnum.Csv:
+                return "*.csv";
+            case SuffixEnum.Txt:
+                return "*.txt";
+            case SuffixEnum.Lua:
+                return "*.lua";
+            default:
+                return "null";
+        }
+    }
+
+    public static SuffixEnum StringToSuffixEnum(string s)
+    {
+        switch (s)
+        {
+            case "*.prefab":
+                return SuffixEnum.Prefab;
+            case "*.png":
+                return SuffixEnum.Png;
+            case "*.csv":
+                return SuffixEnum.Csv;
+            case "*.txt":
+                return SuffixEnum.Txt;
+            case "*.lua":
+                return SuffixEnum.Lua;
+            default:
+                return SuffixEnum.Prefab;
+        }
+    }
+
+    public static string VariantEnumToString(VariantEnum se)
+    {
+        switch (se)
+        {
+            case VariantEnum.None:
+                return "none";
+            case VariantEnum._1280x720:
+                return "1280x720";
+            default:
+                return "none";
+        }
+    }
+
+    public static VariantEnum StringToVariantEnum(string s)
+    {
+        switch (s)
+        {
+            case "none":
+                return VariantEnum.None;
+            case "1280x720":
+                return VariantEnum._1280x720;
+            default:
+                return VariantEnum.None;
+        }
+    }
+
+    void Save()
+    {
+        string path = Application.dataPath + "/Files/BuildMap_cvs/AssetBundleInfo.csv";
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < count; i++)
+        {
+            if (string.IsNullOrEmpty(bundleNameList[i])) break;
+            sb.Append(bundleNameList[i] + ",");
+            sb.Append(SuffixEnumToString(suffixList[i]) + ",");
+            sb.Append(pathList[i] + ",");
+            sb.Append(VariantEnumToString(variant) + "\r\n");
+        }
+        File.WriteAllText(path, sb.ToString());
+        AssetDatabase.Refresh();
+        Clear();
+    }
+}
---
+++ PackagerUtils.cs
@@ -0,0 +1,320 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.IO;
+
+public static class PackagerUtils
+{
+    /// <summary>
+    /// 数据目录
+    /// </summary>
+    static string AppDataPath
+    {
+        get { return Application.dataPath.ToLower(); }
+    }
+
+    /// <summary>
+    /// AssetBundleBuild list
+    /// </summary>
+    static List<AssetBundleBuild> maps = new List<AssetBundleBuild>();
+
+    /// <summary>
+    /// 路径字符串 list
+    /// </summary>
+    static List<string> paths = new List<string>();
+
+    /// <summary>
+    /// 文件名字符串 list
+    /// </summary>
+    static List<string> files = new List<string>();
+
+    /// <summary>
+    /// 打包非 Lua AssetBundle 资源
+    /// </summary>
+    public static void BuildAssetResource(bool deleteExists, BuildTarget target)
+    {
+        // 如果数据存放目录已经存在则删除它 (pc为"c:/" + 项目名 + "/")
+        if (Directory.Exists(PathUtils.DataPath))
+        {
+            Directory.Delete(PathUtils.DataPath, true);
+        }
+
+        // 获取 streaming 目录(项目目录/Assets/StreamingAssets)，如果已经存在则删除它
+        string streamPath = Application.streamingAssetsPath;
+        if (deleteExists)
+        {
+            // 如果 streaming 目录存在，就删除并重新创建，并刷新 AssetDatabase
+            if (Directory.Exists(streamPath))
+            {
+                Directory.Delete(streamPath, true);
+            }
+
+            Directory.CreateDirectory(streamPath);
+            AssetDatabase.Refresh();
+        }
+
+        // 重新读取 maps
+        maps.Clear();
+        HandleBundle();
+
+        /* ------------------------------------------------------*/
+        /* Variant 问题有可能是 5.3.x 的 bug 不影响使用，经验证确实不影响使用 */
+        /* ------------------------------------------------------*/
+
+        // 获取资源地址（/Assets/StreamingAssets）
+        string resPath = "Assets/" + AppDefine.AssetDir;
+        // BuildAssetBundleOptions ： 在创建时不编译 | 哈希 id
+        BuildAssetBundleOptions options =
+            BuildAssetBundleOptions.DeterministicAssetBundle |
+            BuildAssetBundleOptions.UncompressedAssetBundle;
+        // 创建所有 asset
+        BuildPipeline.BuildAssetBundles(resPath, maps.ToArray(), options, target);
+
+        // 新建文件，写入路径及 md5 相关信息
+        BuildFileIndex();
+
+        // 刷新
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 处理非Lua包
+    /// </summary>
+    static void HandleBundle()
+    {
+        string resPath = AppDataPath + "/" + AppDefine.AssetDir + "/";
+        if (!Directory.Exists(resPath)) Directory.CreateDirectory(resPath);
+
+        string content = File.ReadAllText(Application.dataPath + "/Files/BuildMap_cvs/AssetBundleInfo.csv");
+
+        string[] contents = content.Split(
+            new string[] { "\r\n" },
+            System.StringSplitOptions.RemoveEmptyEntries);
+
+        for (int i = 0; i < contents.Length; i++)
+        {
+            string[] a = contents[i].Split(',');
+
+            AddBuildMap(a[0], a[1], a[2], a[3]);
+        }
+    }
+
+    /// <summary>
+    /// 打包 Lua AssetBundle 资源
+    /// </summary>
+    public static void BuildLuaResource(bool all, bool deleteExists, BuildTarget target)
+    {
+        // 如果数据存放目录已经存在则删除它 (pc为"c:/" + 项目名 + "/")
+        if (Directory.Exists(PathUtils.DataPath))
+        {
+            Directory.Delete(PathUtils.DataPath, true);
+        }
+
+        // 获取 streaming 目录(项目目录/Assets/StreamingAssets)，如果已经存在则删除它
+        string streamPath = Application.streamingAssetsPath;
+        if (deleteExists)
+        {
+            // 如果 streaming 目录存在，就删除并重新创建，并刷新 AssetDatabase
+            if (Directory.Exists(streamPath))
+            {
+                Directory.Delete(streamPath, true);
+            }
+
+            Directory.CreateDirectory(streamPath);
+            AssetDatabase.Refresh();
+        }
+
+        // 重新读取 maps
+        maps.Clear();
+        HandleLuaBundle(all);
+
+        // 获取资源地址（/Assets/StreamingAssets）
+        string resPath = "Assets/" + AppDefine.AssetDir;
+        // BuildAssetBundleOptions ： 在创建时不编译 | 哈希 id
+        BuildAssetBundleOptions options =
+            BuildAssetBundleOptions.DeterministicAssetBundle |
+            BuildAssetBundleOptions.UncompressedAssetBundle;
+        // 创建所有 asset
+        BuildPipeline.BuildAssetBundles(resPath, maps.ToArray(), options, target);
+
+        // 新建文件，写入路径及 md5 相关信息
+        BuildFileIndex();
+
+        // 拼接 lua 临时目录
+        string luaTempDir = Application.dataPath + AppDefine.LuaTempDir;
+        // 删除 lua 临时目录
+        if (Directory.Exists(luaTempDir)) Directory.Delete(luaTempDir, true);
+        // 刷新
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 处理Lua包
+    /// </summary>
+    static void HandleLuaBundle(bool all)
+    {
+        string luaTempDir = Application.dataPath + AppDefine.LuaTempDir;
+        if (!Directory.Exists(luaTempDir)) Directory.CreateDirectory(luaTempDir);
+
+        string content = File.ReadAllText(Application.dataPath + "/Files/BuildMap_cvs/AssetBundleInfo.csv");
+
+        string[] contents = content.Split(
+            new string[] { "\r\n" },
+            System.StringSplitOptions.RemoveEmptyEntries);
+
+        string dir = Path.GetDirectoryName(contents[0].Split(',')[2]);
+        string _name = contents[0].Split(',')[0];
+
+        CopyLuaBytesFiles(dir, luaTempDir);
+
+        if (all)
+        {
+            string[] dirs = Directory.GetDirectories(luaTempDir, "*", SearchOption.AllDirectories);
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                string name = dirs[i].Replace(luaTempDir, string.Empty);
+                name = name.Replace('\\', '_').Replace('/', '_');
+                name = "lua/lua_" + name.ToLower() + AppDefine.ExtName;
+                string path = "Assets" + dirs[i].Replace(Application.dataPath, "");
+                AddBuildMap(name, "*.bytes", path);
+            }
+        }
+        else
+        {
+            string[] dirs = Directory.GetDirectories(luaTempDir, "*", SearchOption.AllDirectories);
+            string newPath = "";
+            for (int i = 0; i < dirs.Length; i++)
+            {
+                string[] tempDir = dirs[i].Split('/');
+                string[] tempName = _name.Split(
+                new string[] { "." },
+                System.StringSplitOptions.RemoveEmptyEntries);
+                //Debug.Log(tempDir[tempDir.Length - 1]);
+                //Debug.Log(tempName[0]);
+                if (tempDir[tempDir.Length - 1].ToLower() == tempName[0])
+                {
+                    newPath = "Assets" + dirs[i].Replace(Application.dataPath, "");
+                }
+            }
+            AddBuildMap("lua/lua_" + _name + AppDefine.ExtName, "*.bytes", newPath);
+        }
+
+        AddBuildMap("lua/lua" + AppDefine.ExtName, "*.bytes", "Assets" + AppDefine.LuaTempDir);
+        AssetDatabase.Refresh();
+    }
+
+    /// <summary>
+    /// 将参数赋值给新建的 AssetBundleBuild 实例并添加到 list “maps"中去
+    ///  AssetBundleBuild 类型可作为 BuildPipeline.BuildAssetBundles 方法的参数
+    ///  （有多个需要传递可以新建为数组）将需要打包的文件信息传递进方法进行打包
+    /// </summary>
+    static void AddBuildMap(string bundleName, string pattern, string path, string bundleVariant = null)
+    {
+        // 获取符合条件的文件的文件名，如果没有获取到就退出
+        string[] files = Directory.GetFiles(path, pattern);
+        if (files.Length == 0) return;
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            files[i] = files[i].Replace('\\', '/');
+        }
+
+        AssetBundleBuild build = new AssetBundleBuild();
+        build.assetBundleName = bundleName;
+        if (bundleVariant != "none")
+        {
+            build.assetBundleVariant = bundleVariant;
+        }
+        build.assetNames = files;
+        maps.Add(build);
+    }
+
+    /// <summary>
+    /// 创建与服务器对比用的 flies.txt，写入路径和 md5
+    /// </summary>
+    static void BuildFileIndex()
+    {
+        // 获取 StreamingAssets 路径
+        string resPath = AppDataPath + "/StreamingAssets/";
+        // 拼接完整文件路径字符串
+        string newFilePath = resPath + "/files.txt";
+        // 如果已经存在就先删除
+        if (File.Exists(newFilePath)) File.Delete(newFilePath);
+
+        // 清空 List<string> paths，List<string> files
+        paths.Clear();
+        files.Clear();
+        // 遍历目录及其子目录,并格式化 string 为正确格式，更新到 paths list
+        Recursive(resPath);
+
+        // 在 newFilePath 路径创建一个新文件
+        FileStream fs = new FileStream(newFilePath, FileMode.CreateNew);
+        StreamWriter sw = new StreamWriter(fs);
+        for (int i = 0; i < files.Count; i++)
+        {
+            // 获取当前字符串
+            string file = files[i];
+            // 获取当前字符串路径文件的扩展名
+            //string ext = Path.GetExtension(file);
+            // 如果是 .meta 或 .DS_Store 文件，跳过
+            if (file.EndsWith(".meta") || file.Contains(".DS_Store")) continue;
+
+            // 计算当前字符串的 md5 值
+            string md5 = Md5Utils.md5file(file);
+            // 获取当前字符串删除与 resPath 相同的字符后的结果
+            string value = file.Replace(resPath, string.Empty);
+
+            // 将 value 和 "|" 拼接 md5 写入到新文件中
+            sw.WriteLine(value + "|" + md5);
+        }
+        sw.Close();
+        fs.Close();
+    }
+
+    /// <summary>
+    /// 遍历目录及其子目录,并格式化 string 为正确格式，更新到 paths list
+    /// </summary>
+    static void Recursive(string path)
+    {
+        string[] names = Directory.GetFiles(path);
+        string[] dirs = Directory.GetDirectories(path);
+        foreach (string filename in names)
+        {
+            string ext = Path.GetExtension(filename);
+            if (ext.Equals(".meta")) continue;
+            files.Add(filename.Replace('\\', '/'));
+        }
+        foreach (string dir in dirs)
+        {
+            paths.Add(dir.Replace('\\', '/'));
+            Recursive(dir);
+        }
+    }
+
+    public static void CopyLuaBytesFiles(string sourceDir, string destDir, bool appendext = true)
+    {
+        if (!Directory.Exists(sourceDir))
+        {
+            return;
+        }
+
+        // 返回目录及其子目录下所有lua文件名
+        string[] files = Directory.GetFiles(sourceDir, "*.lua", SearchOption.AllDirectories);
+
+        int len = sourceDir.Length;
+        if (sourceDir[len - 1] == '/' || sourceDir[len - 1] == '\\')
+        {
+            --len;
+        }
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            string str = files[i].Remove(0, len);
+            string dest = destDir + str;
+            if (appendext) dest += ".bytes";
+            string dir = Path.GetDirectoryName(dest);
+            Directory.CreateDirectory(dir);
+            File.Copy(files[i], dest, true);
+        }
+    }
+}
